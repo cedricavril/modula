7bcc387a2ca5fe559d14d0f43fc834c524e14484
Cédric <cedric_avril_pro@hotmail.fr>
2020-02-28 13:43:37 +0100

    last commit before sending

--- admin/index.php
+++ admin/index.php
@@ -1,50 +1,64 @@
+<?php 
+  session_start();
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr-FR">
 <head>
   <title>PPP - admin</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<!-- will be worthy to chop bs here too lately -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
   <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-
-
 <?php
-  $error = "";
-  require('../back-end/private/PDOFactory.class.php');
+  if ($_SERVER['REMOTE_ADDR'] === '127.0.0.1')
+    require('../back-end/private/PDOFactory.local.class.php');
+  else
+    require('../back-end/private/PDOFactory.class.php');
   try {
     $db = PDOFactory::getMysqlConnexion();
-    } catch (PDOException $e) {
-        $error = $e->getMessage();
-        echo $error;
-        die();
-    }
+  } catch (PDOException $e) {
+    echo $e->getMessage();
+    die();
+  }
 ?>
 
+  <div class="container">
 
-<div class="container">
-  <h2>Liste des formulaires de contact ayant été envoyés</h2>
-<?php $stmt = $db->query("SELECT DATE_FORMAT(date_envoi, '%d/%m/%Y') as dateEnvoi, DATE_FORMAT(date_envoi, '%Hh%imin%ss') as heureEnvoi, email FROM modula_users ORDER BY date_envoi DESC"); 
-  while ($row = $stmt->fetch()) { ?>
-    <ul class="list-group list-group-horizontal">
-      <li class="list-group-item"><?php echo explode("@", $row['email'])[0] . "<br>@" . explode("@", $row['email'])[1]; ?></li>
-      <li class="list-group-item"><?= $row['dateEnvoi']; ?></li>
-      <li class="list-group-item"><?= $row['heureEnvoi']; ?></li>
-    </ul>
-  <?php } ?>
-</div>
-<div class="container">
-  <h2>Dernier envoi réalisé</h2>
-<?php 
-  $stmt = $db->query("SELECT nom, prenom, DATE_FORMAT(date_envoi, '%d/%m/%Y') as dateEnvoi, DATE_FORMAT(date_envoi, '%Hh%imin%ss') as heureEnvoi, email, message FROM modula_users ORDER BY date_envoi DESC LIMIT 1"); 
-  $row = $stmt->fetch();
-  echo $row['email'] . ', ' . $row['nom'] . ', ' . $row['prenom'] . ', ' . $row['dateEnvoi'] . ', ' . $row['heureEnvoi'] . ', ' . $row['email'] . ', ' . $row['message']; ?>
-  </ul>
-</div>
+  <?php $stmt = $db->query("SELECT id, DATE_FORMAT(date_envoi, '%d/%m/%Y') as dateEnvoi, DATE_FORMAT(date_envoi, '%Hh%imin%ss') as heureEnvoi, email FROM modula_users ORDER BY date_envoi DESC"); 
+    $row = $stmt->fetch();
+    if (!$row) {
+      echo "Aucun envoi réalisé";
+      exit;
+    } else {
+      include "connectionScript.php";
+      if($_POST) include "check.php"; 
+    }?>
 
+    <h2>Liste des formulaires de contact ayant été envoyés : (<u style=" white-space: nowrap;">Cliquer sur la date pour les détails</u>)</h2>
+
+  <?php
+  $i = 0; 
+  do {
+    $i++; ?>
+      <ul class="list-group list-group-horizontal">
+        <li class="list-group-item"><?= $row['email']; ?></li>
+        <li class="list-group-item">
+          <form id="details<?= $i ?>" action="#" method="POST">
+            <input type="hidden" name="id" value="<?= $row['id'] ?>"/> 
+            <a href='#' onclick='document.getElementById("details<?= $i ?>").submit()'><?= $row['dateEnvoi']; ?></a>
+          </form> 
+        </li>
+        <li class="list-group-item"><?= $row['heureEnvoi']; ?></li>
+      </ul>
+    <?php } while ($row = $stmt->fetch()) ?>
+  </div>
+<script type="text/javascript">
+  document.getElementById("connection-form").style.display = "none";
+</script>
 </body>
 </html>
--- modula_users.sql
+++ modula_users.sql
@@ -9,3 +9,15 @@ CREATE TABLE IF NOT EXISTS `modula_users` (
   `date_envoi` timestamp NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `modula_administrators`;
+CREATE TABLE IF NOT EXISTS `modula_administrators` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` mediumtext NOT NULL,
+  `pass` mediumtext NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+);
+
+
+INSERT INTO `modula_administrators` (`nom`, `pass`) 
+VALUES ('admin', '$1$Ae4Q7OdQ$VOYEYAUOMSPPoxXpcUd9A/');
